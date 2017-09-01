46915847f8972db8afd075db066e4c5ae007d3d9
Feng <ingf.ivan@gmail.com>
2017-09-01 13:12:08 +0800

    :wrench: add eslint config

---
+++ ios/EventManager.h
@@ -0,0 +1,13 @@
+//
+//  EventManager.h
+//  rnDemo
+//
+//  Created by inf on 1/9/17.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EventManager : NSObject
+
+@end
---
+++ ios/EventManager.m
@@ -0,0 +1,13 @@
+//
+//  EventManager.m
+//  rnDemo
+//
+//  Created by inf on 1/9/17.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import "EventManager.h"
+
+@implementation EventManager
+
+@end
